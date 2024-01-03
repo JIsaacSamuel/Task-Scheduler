c07d033033b3f2c1d3121cc0f658d509b68f701e
JIsaacSamuel <isij2018@gmail.com>
2024-01-03 20:37:36 +0530

    Connected db. Nothing else done

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('This is main.js file')
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/task-route.js');
+
+// const agenda = require('agenda');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use('', router)
+
+// Connect to MongoDB
+try {
+    mongoose.connect('mongodb://localhost/task_scheduler_database', { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log('Connection to database established')
+    db = mongoose.connection
+} catch (err) {
+    console.log(`Error occured: ${err}`)
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
